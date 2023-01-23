799982a980e607b58e31eb4768e7d77e84d132bc
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-01-23 21:04:29 +0300

    Bolum 8 Cart ve Siparisler

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -49,14 +49,18 @@ const Header = () => {
           )}
         </nav>
         <div className="flex gap-x-4 items-center">
-          <a href="#">
-            <FaUserAlt className="hover:text-primary transition-all" />
-          </a>
-          <a href="#">
-            <FaShoppingCart className="hover:text-primary transition-all" />
-          </a>
+          <Link href="/auth/login">
+            <span>
+              <FaUserAlt className="hover:text-primary transition-all cursor-pointer" />
+            </span>
+          </Link>
+          <Link href="/cart">
+            <span>
+              <FaShoppingCart className="hover:text-primary transition-all cursor-pointer" />
+            </span>
+          </Link>
           <button onClick={() => setIsSearchModal(true)}>
-            <FaSearch className="hover:text-primary transition-all" />
+            <FaSearch className="hover:text-primary transition-all cursor-pointer" />
           </button>
           <a href="#" className="md:inline-block hidden sm">
             <button className="btn-primary">Order Online</button>
