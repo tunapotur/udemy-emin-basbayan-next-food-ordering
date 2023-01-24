be414447dd18b22b6332fec6e7e7a0ba1df1f1cf
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-01-24 17:46:23 +0300

    Redux Corrections

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
